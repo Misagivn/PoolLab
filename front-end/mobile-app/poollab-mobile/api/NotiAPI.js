afca319fe1c060c8edc32505cc654ee3d6014252
Minh Nhat Luong <75418809+Misagivn@users.noreply.github.com>
2025-01-11 14:34:52 +0700

    noti

---
+++ front-end/mobile-app/poollab-mobile/api/NotiAPI.js
@@ -0,0 +1,23 @@
+import ApiManager from "./ApiManager";
+
+export const getNotReadNoti = async (customerId) => {
+  try {
+    const response = await ApiManager.get(
+      `/notification/countnotinotread/${customerId}`
+    );
+    return response;
+  } catch (error) {
+    if (error.response) {
+      // Server responded with an error status (4xx, 5xx)
+      return error.response;
+    } else if (error.request) {
+      // Request was made but no response received
+      console.log("Request error:", error.request);
+      return error.request;
+    } else {
+      // Error occurred while setting up the request
+      console.log("Error:", error.message);
+      return error;
+    }
+  }
+};
